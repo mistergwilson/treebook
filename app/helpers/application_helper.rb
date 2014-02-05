a31ac85d9b13b470c946455836af9663534c3804
Gregory Wilson <mistergwilson@gmail.com>
2014-02-05 15:00:53 -0500

    Styled the flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	#determine which alert styling to use
+	def flash_class(type)
+		case type
+		when :alert
+			"alert-danger"
+		when :notice
+			"alert-sucess"
+		else
+			""
+		end
+    end
 end
