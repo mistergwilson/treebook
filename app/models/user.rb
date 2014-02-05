70bc3aca1031de9d6aa78a83172be84d4574e54e
Gregory Wilson <mistergwilson@gmail.com>
2014-02-05 14:25:32 -0500

    Added tests for user first name, last name, and profile correctness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates :user_name, presence: true, uniqueness: true, format: { with: /a-zA-Z0-9_-/, message: 'Please use only letters, numbers, underscores, or dashes in username.'}
+
   has_many :statuses 
   
   def full_name
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,41 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+   test "a user should enter a first name" do
+   		user = User.new
+   		assert !user.save
+   		assert !user.errors[:first_name].empty?
+   	end
+
+   	test "a user should enter a last name" do
+   		user = User.new
+   		assert !user.save
+   		assert !user.errors[:last_name].empty?
+   	end
+
+   	test "a user should enter a user name" do
+   		user = User.new
+   		assert !user.save
+   		assert !user.errors[:user_name].empty?
+   	end
+
+   	test "a user should have a unique profile name" do
+   		user = User.new
+   		user.user_name = users(:gregory).user_name
+
+   		#don't save user to database
+   		assert !user.save
+   		#view log
+   		puts user.errors.inspect
+   		assert !user.errors[:user_name].empty?
+   	end
+
+   	test "a user should have a user name without spaces" do
+   		user = User.new
+   		user.user_name = "My profile with spaces"
+
+   		assert !user.save
+   		assert !user.errors[:user_name].empty?
+   		assert user.errors[:user_name].include?("Please use only letters, numbers, underscores, or dashes in username.")
+   	end
 end
