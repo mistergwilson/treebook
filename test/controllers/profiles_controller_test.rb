826d77333a1a62747cdf926223c9334b85b69ceb
Gregory Wilson <mistergwilson@gmail.com>
2014-02-06 22:59:39 -0500

    Added Gravatars to statuses

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -4,10 +4,11 @@ class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
     get :show, id: users(:gregory).user_name
     assert_response :success
+    assert_template 'profiles/show'
   end
 
   test "that variables are assigned on successful profile viewing" do
-  	get :show. id: users(:gregory).user_name
+  	get :show, id: users(:gregory).user_name
   	assert assigns(:user)
   	assert_not_empty assigns(:statuses)
   end
@@ -16,6 +17,7 @@ class ProfilesControllerTest < ActionController::TestCase
   	get :show, id: users(:gregory).user_name
   	assigns(:statuses).each do |status|
   		assert_equal users(:gregory), status.user
+  	end
   end
-
+  
 end
