af48344221bae18c12c45c131a140f33773b919a
Gregory Wilson <mistergwilson@gmail.com>
2014-02-08 20:45:08 -0500

    Scoped status updates to the current user

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -29,13 +29,15 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
-  test "should create status when logged in" do
+  test "should create status for current user when logged in" do
     sign_in users(:gregory)
+
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content}
+      post :create, status: { content: @status.content, user_id: users(:jim).id }
     end
 
     assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:gregory).id
   end
 
   test "should show status" do
